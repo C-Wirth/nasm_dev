8b39d38d5350ba541ff8f34f3e7541ee15b9cd6c
Colby Harrison Wirth <colby.wirth@maine.edu>
2025-04-21 15:56:08 -0400

    program fixed

--- old_lab3.asm
+++ old_lab3.asm
@@ -22,11 +22,6 @@ section .data
     exitPrompt_len equ $-exitPrompt 
 
     subtraction_flag db 0 ; flag for determining addition or subtraction, 0 = add, 1 = subtract
-    is_neg_flag db 0
-
-    newline db 13, 10
-    newline_len equ $-newline
-
 
 section .bss
     stdHandle resd 1
@@ -34,8 +29,7 @@ section .bss
     user_input_buf resb 4
     operand1 resd 1
     operand2 resd 1
-    result_buf resb 5
-    output_size resb 1 ; 1 byte to store the output size
+    result_buf resb 5 
 
 section .text ;APIs
     extern _GetStdHandle@4, _WriteConsoleA@20, _ReadConsoleA@20, _ExitProcess@4
@@ -75,7 +69,7 @@ loop_start:
     je exit_process
     
     cmp eax, '2'
-    sete [subtraction_flag] ; sets subtraction flag
+    setne [subtraction_flag] ; sets subtraction flag
     xor eax, eax
 
     ;;; next, get operands 1 and 2 ;;;
@@ -136,7 +130,7 @@ loop_start:
     mov ebx, [operand2]
 
     ;get operation type
-    cmp byte [subtraction_flag], 1
+    cmp byte [subtraction_flag], 0
     je subtraction_block
 
     addition_block:
@@ -145,68 +139,100 @@ loop_start:
 
     subtraction_block:
     SUB eax, ebx
-    jmp convert_to_ASCII
-
 
     convert_to_ASCII:
 
         xor ecx, ecx ;ecx used for loop
-        xor edx, edx
         mov ebx, 10 ; dividing by 10 to get next digit in loop
-        mov esi, 0 ; will use for reversing the digits
-        mov edi, result_buf 
+        mov esi, result_buf ; stores the result
 
-        cmp eax, 0
-        jge convert_loop ; else condition, the value is negative
-        mov byte [is_neg_flag], 1 ; account for the '-'
-        neg eax
+        cmp eax, 9
+        jg convert__two_digit_loop ; two digit positive numbers
+
+        cmp eax, -1 
+        jg convert_single_digit ; single digit positive numbers
+
+        cmp eax, -9 
+        jl convert_large_neg ; two digit negative numbers
+
+        ;else this has to be a single digit negative number
+            mov byte [result_buf], '-'
+            inc esi
+            inc ecx
+            neg eax
+            jmp convert_single_digit
 
 
-    convert_loop:
-    
+    convert_large_neg:
+    mov byte [result_buf], '-'
+    inc esi
+    inc ecx
+    neg eax
+    jmp convert__two_digit_loop
+
+
+    convert_single_digit:
+        add eax, '0'
+        mov [result_buf+esi], al
+        inc ecx
+        jmp done
+
+
+    convert__two_digit_loop:
         xor edx, edx
         div ebx ; divide EAX by 10
-        add dl, '0'
-        mov [edi + esi], dl
-        inc esi
+        add edx, '0'
+        push edx
         inc ecx
         test eax, eax ; check quotient
-        jnz convert_loop
+        jnz convert__two_digit_loop
 
-        ; deal with negative numbers
-        cmp byte [is_neg_flag], 0
-        je skip_neg
-        mov byte [edi + esi], '-' 
-        inc esi
-        inc ecx
-
-    skip_neg:
-        mov [output_size], ecx
-
-        ; utilizing eax and ebx track reversing while eax < ebx
-        mov eax, 0
-        mov ebx, ecx 
-        dec ebx                
-
-
+        mov esi, 0
     reverse_elements_loop:
+        pop edx
+        mov [result_buf+esi], edx
+        inc esi
+        loop reverse_elements_loop
 
-        cmp eax, ebx ; check if eax and ebx have not passed eachother
-        jge done
 
-        ; swap elements
-        mov dl, [edi + eax]
-        mov dh, [edi + ebx]
-        mov [edi + eax], dh
-        mov [edi + ebx], dl
-
-        ;increment and decrement pointers
-        inc eax
-        dec ebx
-        jmp reverse_elements_loop
-
-    
     done:
+
+        ; movzx edx, byte [eax+ecx]
+        ; cmp edx, '
+        ; jl done
+        ; cmp edx, '9'
+
+
+
+
+       ; cmp eax, 0
+       ; jl convert_to_negative_str
+        ;jg
+        ;jmp convert_to_positive_str
+
+       ; convert_to_positive_str:
+      ;  cmp eax, 10
+     ;   jl print_val
+        ;;;; convert vals > 10 here logic here ;;;;
+    ;    ADD eax, '0'
+
+   ;      convert_to_negative_str:
+  ;       cmp eax, -10
+ ;        jl convert_two_neg_digits
+         ;;; convert negative vals > -10 here ;;;;
+
+;         convert_two_neg_digits:
+        ;;; convert vals < -10 here ;;;
+
+    ;print_val:
+   ;     ADD eax, '0'
+  ;      mov [result_buf], al
+ ;       mov byte [result_buf+1], 10
+;        xor ecx, ecx
+
+
+
+
 
     write_output:
     push -11
@@ -224,23 +250,10 @@ loop_start:
     call _GetStdHandle@4
     mov [stdHandle], eax
     push 0
-    push dword [output_size]
+    push 2
     push result_buf
     push dword [stdHandle]
     call _WriteConsoleA@20
-
-    ; write newline after result
-    push -11
-    call _GetStdHandle@4
-    mov [stdHandle], eax
-
-    push 0
-    push newline_len
-    push newline
-    push dword [stdHandle]
-    call _WriteConsoleA@20
-
-jmp loop_start ; restart the process until the user quits
 
 
 
